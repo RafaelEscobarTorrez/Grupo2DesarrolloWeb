91393c8053f854e1d723be4087cc6ead7f00fed9
RafaelEscobarTorrez <escobartorrezrafael@gmail.com>
2025-06-15 15:20:12 -0400

    15DEJUNIO

--- Laboratorio4-Grupo 2/verificarnivel.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SESSION["nivel"]==0)
-{
-    echo "No tienes permiso para acceder a esta página.";
-    ?>
-    <meta http-equiv="refresh" content="3;url=bandeja.php">
-    <?php
-    die();
-}
